fa691d42a15d82d85750e306e2ddf548ecdb5923
Barrysv <35910877+Barrysv@users.noreply.github.com>
2018-08-11 15:09:26 +0800

    Nearly working - hangs after loading the sensor
    2018-08-11 13:41:47 INFO (MainThread) [homeassistant.setup] Setup of domain sensor took 1.8 seconds.
    2018-08-11 13:41:47 INFO (MainThread) [homeassistant.core] Bus:Handling <Event component_loaded[L]: component=         sensor>
    *** seems to hang here

--- technicolor.py
+++ technicolor.py
@@ -1,7 +1,3 @@
-from homeassistant.const import (STATE_UNKNOWN)
-from homeassistant.helpers.entity import Entity
-from homeassistant.components.sensor import PLATFORM_SCHEMA
-
 from collections import OrderedDict
 import binascii
 import re
@@ -12,18 +8,24 @@ import toml
 import srp
 import datetime
 import time
+import logging
+import voluptuous as vol
 
 from homeassistant.const import (
-    CONF_NAME, CONF_PASSWORD, CONF_USERNAME,
-    CONF_HOST)
-	
+    STATE_UNKNOWN, CONF_NAME, CONF_PASSWORD, CONF_USERNAME,
+    CONF_HOST,CONF_VALUE_TEMPLATE)
+from homeassistant.helpers.entity import Entity
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+import homeassistant.helpers.config_validation as cv
+
 DEFAULT_NAME = 'Technicolor Sensor'
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_HOST): cv.string,
+    vol.Required(CONF_PASSWORD): cv.string,
+    vol.Required(CONF_USERNAME): cv.string,
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-    vol.Optional(CONF_PASSWORD): cv.string,
-    vol.Optional(CONF_USERNAME): cv.string,
+    vol.Optional(CONF_VALUE_TEMPLATE): cv.template,
 })
 
 
@@ -33,25 +35,42 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     username = config.get(CONF_USERNAME)
     password = config.get(CONF_PASSWORD)
     name = config.get(CONF_NAME)
+    value_template = config.get(CONF_VALUE_TEMPLATE)
 
+    if value_template is not None:
+        value_template.hass = hass
 
     fetch = Fetcher({'address':address, 'username':username, 'password':password})
-    fetch.update()
+    fetch.get()
     
-    add_devices([TechnicolorModemSensor(hass, fetch)], True)
+    add_devices([TechnicolorModemSensor(hass, fetch, name, value_template)], True)
     
 class TechnicolorModemSensor(Entity):
     """Representation of a modem Sensor."""
 
-    def __init__(self):
+    def __init__(self, hass, fetch, name, value_template):
         """Initialize the sensor."""
         self._state = STATE_UNKNOWN
         self._name = name
+        self._hass = hass
+        self.fetch = fetch
+        self._unit_of_measurement = None
+        self._value_template = value_template
 
     @property
     def name(self):
         """Return the name of the sensor."""
-        return 'Technicolor Modem Sensor'
+        return self._name
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit the value is expressed in."""
+        return self._unit_of_measurement
+
+    @property
+    def available(self):
+        """Return if the sensor data is available."""
+        return self.fetch.data is not None
 
     @property
     def state(self):
@@ -63,15 +82,6 @@ class TechnicolorModemSensor(Entity):
 
         This is the only method that should fetch new data for Home Assistant.
         """
-        stats_page, gateway_page = fetch.get()
-        stats = OrderedDict()
-        if stats_page:
-            parse_broadband(stats, stats_page)
-        if gateway_page:
-            parse_gateway(stats, gateway_page)
-
-        self._state = json.dumps(stats)
-        
         def fetch_string(soup, title):
             lr = soup.find_all(string=title)
             #D(title)
@@ -117,23 +127,20 @@ class TechnicolorModemSensor(Entity):
             The tg-1 doesn't have id attributes so we have to find text labels.
             """
             soup = BeautifulSoup(html, 'html.parser')
-
-            res['datetime'] = datetime.datetime.now()
+            #res['datetime'] = datetime.datetime.now()
             res['up_rate'], res['down_rate'] = fetch_pair(soup, "Line Rate", 'Mbps')
             res['up_maxrate'], res['down_maxrate'] = fetch_pair(soup, "Maximum Line rate", 'Mbps')
             res['up_power'], res['down_power'] = fetch_pair(soup, "Output Power", 'dBm')
             res['up_noisemargin'], res['down_noisemargin'] = fetch_pair(soup, "Noise Margin", 'dB')
-            res['up_transferred'], res['down_transferred'] = fetch_pair(soup, "Data Transferred", "MBytes")
-            fetch_line_attenuation(soup, res)
+            #res['up_transferred'], res['down_transferred'] = fetch_pair(soup, "Data Transferred", "MBytes")
+            #fetch_line_attenuation(soup, res)
             res['dsl_uptime'] = fetch_uptime(soup ,'DSL Uptime')
-            res['dsl_mode'] = fetch_string(soup, 'DSL Mode')
-            res['dsl_type'] = fetch_string(soup, 'DSL Type')
+            #res['dsl_mode'] = fetch_string(soup, 'DSL Mode')
+            #res['dsl_type'] = fetch_string(soup, 'DSL Type')
             res['dsl_status'] = fetch_string(soup, 'DSL Status')
-
             # integer kbps are easier to work with in scripts
             for n in 'down_rate', 'up_rate', 'down_maxrate', 'up_maxrate':
                 res[n] = int(res[n] * 1000)
-
             return res
 
         def parse_gateway(res, html):
@@ -150,17 +157,27 @@ class TechnicolorModemSensor(Entity):
             ]
             for n in names:
                 res[n.lower().replace(' ', '_')] = fetch_string(soup, n)
-
             res['uptime'] = fetch_uptime(soup, 'Uptime')
-			
-			
-			
+         
+        self.fetch.get()
+        #stats_page, gateway_page = self.fetch.data
+        stats_page = self.fetch.data
+        stats = OrderedDict()
+        if stats_page:
+            parse_broadband(stats, stats_page)
+        #if gateway_page:
+        #    parse_gateway(stats, gateway_page)
+        #print('raw dump')
+        #print('\n'.join('%s %s' % (str(k), str(v)) for k, v in stats.items()))
+        self._state = json.dumps(stats, indent=4)
+
 class Fetcher(object):
-	"""Class for handling the data retrieval."""
+    """Class for handling the data retrieval."""
     def __init__(self, config):
         self.config = config
         self.top_url = 'http://%s' % self.config['address']
         self.session = None
+        self.data = None
 
     def connect(self):
         """ Authenticates with the modem. 
@@ -237,10 +254,11 @@ class Fetcher(object):
         modem_url = '%s/modals/broadband-bridge-modal.lp' % self.top_url
         r = self.session.get(modem_url)
 
-        gateway_url = '%s/modals/gateway-modal.lp' % self.top_url
-        g = self.session.get(gateway_url)
+        #gateway_url = '%s/modals/gateway-modal.lp' % self.top_url
+        #g = self.session.get(gateway_url)
 
-        return r.text, g.text
+        #self.data = r.text, g.text
+        self.data = r.text
 		
 		
 # Special thanks to Matt Johnston
